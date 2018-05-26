df29a53932cef5c6deffc2f87eb13a7556fdb228
engineerthoughrts <32882842+engineerthoughts@users.noreply.github.com>
2018-05-26 19:55:52 +0530

    code tnpsc

---
+++ code.js
@@ -0,0 +1,212 @@
+function codevalidate1() 
+		{
+            if (document.getElementById("login").value == "5050") 
+			{
+				openCity(event, 'question1');
+            } else if (document.getElementById("login").value == "01052209")
+			{
+                openCity(event, 'question1');
+            } else
+			{
+				alert("Please Enter the Valid Code");
+			}
+				
+        }
+		
+var myFuncCalls = 0;
+      function fnChangeBorder(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans1").style.border = "solid #66cc33";
+       document.getElementById("B").disabled = true;
+       document.getElementById("C").disabled = true;
+       document.getElementById("D").disabled = true;
+       document.getElementById("ans").disabled = true;
+      }
+      function answervalidate() 
+      {
+       document.getElementById("ans1").style.border = "solid #66cc33";
+       document.getElementById("B").disabled = true;
+       document.getElementById("C").disabled = true;
+       document.getElementById("D").disabled = true;
+       document.getElementById("ans1").disabled = true;
+       myFuncCalls++;
+      }
+      //2
+      function fnChangeBorder1(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans2").style.border = "solid #66cc33";
+       document.getElementById("B1").disabled = true;
+       document.getElementById("C1").disabled = true;
+       document.getElementById("D1").disabled = true;
+       document.getElementById("ans2").disabled = true;
+      }
+      function answervalidate1() 
+      {
+       document.getElementById("ans2").style.border = "solid #66cc33";
+       document.getElementById("B1").disabled = true;
+       document.getElementById("C1").disabled = true;
+       document.getElementById("D1").disabled = true;
+       document.getElementById("ans2").disabled = true;
+       myFuncCalls++;
+      }
+      //3
+      function fnChangeBorder2(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans3").style.border = "solid #66cc33";
+       document.getElementById("B2").disabled = true;
+       document.getElementById("C2").disabled = true;
+       document.getElementById("D2").disabled = true;
+       document.getElementById("ans3").disabled = true;
+      }
+      function answervalidate2() 
+      {
+       document.getElementById("ans3").style.border = "solid #66cc33";
+       document.getElementById("B2").disabled = true;
+       document.getElementById("C2").disabled = true;
+       document.getElementById("D2").disabled = true;
+       document.getElementById("ans3").disabled = true;
+       myFuncCalls++;
+      }
+      //4
+      function fnChangeBorder3(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans4").style.border = "solid #66cc33";
+       document.getElementById("B10").disabled = true;
+       document.getElementById("C10").disabled = true;
+       document.getElementById("D10").disabled = true;
+       document.getElementById("ans4").disabled = true;
+      }
+      function answervalidate3() 
+      {
+       document.getElementById("ans4").style.border = "solid #66cc33";
+       document.getElementById("B10").disabled = true;
+       document.getElementById("C10").disabled = true;
+       document.getElementById("D10").disabled = true;
+       document.getElementById("ans4").disabled = true;
+       myFuncCalls++;
+      }
+      
+      //5
+      function fnChangeBorder4(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans5").style.border = "solid #66cc33";
+       document.getElementById("B4").disabled = true;
+       document.getElementById("C4").disabled = true;
+       document.getElementById("D4").disabled = true;
+       document.getElementById("ans5").disabled = true;
+      }
+      function answervalidate4() 
+      {
+       document.getElementById("ans5").style.border = "solid #66cc33";
+       document.getElementById("B4").disabled = true;
+       document.getElementById("C4").disabled = true;
+       document.getElementById("D4").disabled = true;
+       document.getElementById("ans5").disabled = true;
+       myFuncCalls++;
+      }
+      
+      //6
+      function fnChangeBorder5(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans6").style.border = "solid #66cc33";
+       document.getElementById("B5").disabled = true;
+       document.getElementById("C5").disabled = true;
+       document.getElementById("D5").disabled = true;
+       document.getElementById("ans6").disabled = true;
+      }
+      function answervalidate5() 
+      {
+       document.getElementById("ans6").style.border = "solid #66cc33";
+       document.getElementById("B5").disabled = true;
+       document.getElementById("C5").disabled = true;
+       document.getElementById("D5").disabled = true;
+       document.getElementById("ans6").disabled = true;
+       myFuncCalls++;
+      }
+      //7
+      function fnChangeBorder6(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans7").style.border = "solid #66cc33";
+       document.getElementById("B6").disabled = true;
+       document.getElementById("C6").disabled = true;
+       document.getElementById("D6").disabled = true;
+       document.getElementById("ans7").disabled = true;
+      }
+      function answervalidate6() 
+      {
+       document.getElementById("ans7").style.border = "solid #66cc33";
+       document.getElementById("B6").disabled = true;
+       document.getElementById("C6").disabled = true;
+       document.getElementById("D6").disabled = true;
+       document.getElementById("ans7").disabled = true;
+       myFuncCalls++;
+      }
+      //8
+      function fnChangeBorder7(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans8").style.border = "solid #66cc33";
+       document.getElementById("B7").disabled = true;
+       document.getElementById("C7").disabled = true;
+       document.getElementById("D7").disabled = true;
+       document.getElementById("ans8").disabled = true;
+      }
+      function answervalidate7() 
+      {
+       document.getElementById("ans8").style.border = "solid #66cc33";
+       document.getElementById("B7").disabled = true;
+       document.getElementById("C7").disabled = true;
+       document.getElementById("D7").disabled = true;
+       document.getElementById("ans8").disabled = true;
+       myFuncCalls++;
+      }
+      //9
+      function fnChangeBorder8(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans9").style.border = "solid #66cc33";
+       document.getElementById("B8").disabled = true;
+       document.getElementById("C8").disabled = true;
+       document.getElementById("D8").disabled = true;
+       document.getElementById("ans9").disabled = true;
+      }
+      function answervalidate8() 
+      {
+       document.getElementById("ans9").style.border = "solid #66cc33";
+       document.getElementById("B8").disabled = true;
+       document.getElementById("C8").disabled = true;
+       document.getElementById("D8").disabled = true;
+       document.getElementById("ans9").disabled = true;
+       myFuncCalls++;
+      }
+      //10
+      function fnChangeBorder9(boxId)
+      {
+       document.getElementById(boxId).style.border = "solid #ff0000";
+       document.getElementById("ans10").style.border = "solid #66cc33";
+       document.getElementById("B9").disabled = true;
+       document.getElementById("C9").disabled = true;
+       document.getElementById("D9").disabled = true;
+       document.getElementById("ans10").disabled = true;
+      }
+      function answervalidate9() 
+      {
+       document.getElementById("ans10").style.border = "solid #66cc33";
+       document.getElementById("B9").disabled = true;
+       document.getElementById("C9").disabled = true;
+       document.getElementById("D9").disabled = true;
+       document.getElementById("ans10").disabled = true;
+       myFuncCalls++;
+      }
+      function resultsans()
+      {
+      document.getElementById("resultans").innerHTML=myFuncCalls + "/10";
+      }
+      
